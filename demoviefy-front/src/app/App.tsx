263878dd18d44b12a8b445a164740ee8a9b19308
Bruno Timóteo <bruno.timoteo.saber@gmail.com>
2026-06-03 20:11:35 -0300

    Centralizar rotas e títulos das páginas em Router.tsx

--- demoviefy-front/src/app/App.tsx
+++ demoviefy-front/src/app/App.tsx
@@ -1,18 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/Home";
-import Upload from "../pages/Upload";
+import Router from "./Router";
 
 function App() {
   return (
     <BrowserRouter>
       <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="*" element={<Navigate replace to="/" />} />
-        </Routes>
+        <Router />
       </MainLayout>
     </BrowserRouter>
   );
---
+++ demoviefy-front/src/app/Router.tsx
@@ -0,0 +1,35 @@
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "../pages/Home";
+import Upload from "../pages/Upload";
+
+/* Define os títulos das com base nas rotas */
+const titlesMap: Record<string, string> = {
+    "/": "DeMoviefy",
+    "/upload": "DeMoviefy - Painel de Análise"
+};
+
+// Aplica o título na página caso ela esteja declarada acima. Caso não, o valor DeMoviefy padrão será vinculado.
+function TitleManager() {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        document.title =  titlesMap[pathname] ?? "DeMoviefy";
+    }, [pathname]);
+
+    return null;
+}
+
+// Declara todas as rotas disponíveis em nosso projeto. Mais fácil para gerenciamento unificado.
+export default function Router() {
+    return (
+        <>
+            <TitleManager />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/upload" element={<Upload />} />
+                <Route path="*" element={<Navigate replace to="/" />} />
+            </Routes>
+        </>
+    )
+}
